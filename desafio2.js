8fffd71cb1c67efcb701c7a979edb9a05d350bc4
Antonio Carlos <antoniop.andp@gmail.com>
2025-01-04 22:43:13 -0300

    desafio2 concluída

--- desafio2.js
+++ desafio2.js
@@ -1,23 +1,30 @@
 let nivel;
-let nome = "Escuridão";
-let vitorias =2000;
-let derrotas = 100;
+let nome = "Sombra";
+let vitorias = 101;
+let derrotas = 0;
 let saldoVitorias = vitorias - derrotas;
-  
-	if (vitorias < 10) {
-	  nivel = "Ferro";
-	} else if (vitorias <= 20) {
-	  nivel = "Bronze";
-	} else if (vitorias <= 50) {
-	  nivel = "Prata";
-	} else if (vitorias <= 80) {
-	  nivel = "Ouro";
-	} else if (vitorias <= 90) {
-	  nivel = "Diamante";
-	} else if (vitorias <= 100) {
-	  nivel = "Lendário";
-	} else { (vitorias >= 101)
-	  nivel = "Imortal";
-	}
-  
-	console.log("O(a) Herói(a) " + nome + " tem de saldo de " + saldoVitorias + " vitórias está no nível de " + nivel + ".");
+
+function determinarNivel() {
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+}
+
+function exibirStatus() {
+    determinarNivel();
+    console.log("O(a) Herói(a) " + nome + " tem de saldo de " + saldoVitorias + " vitórias está no nível de " + nivel  + ".");
+}
+
+exibirStatus();
